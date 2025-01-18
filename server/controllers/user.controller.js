30a91af8a1b894744940d46b0499a629df8275ab
Pranavaaaa <pranav.22210388@viit.ac.in>
2025-01-18 20:08:01 +0530

    Login Done

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,7 +2,7 @@ const userModel = require('../models/user.model.js');
 const userServices = require('../services/user.services.js');
 const { validationResult } = require('express-validator');
 
-module.exports.register = async (req, res, next) => {
+module.exports.registerUser = async (req, res, next) => {
     try {
       const errors = validationResult(req);
 
@@ -32,3 +32,34 @@ module.exports.register = async (req, res, next) => {
       next(error);
     }
 };
+
+module.exports.loginUser = async (req, res, next) => {
+    try{
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()){
+          return res.status(400).json({ error: errors.array() });
+        }
+
+        const { email, password } = req.body;
+
+        const user = await userModel.findOne({ email }).select(' +password ');
+
+        if(!user){
+          return res.status(401).json({ message: 'Invalid email or password'});
+        }
+
+        const isMatch = await user.comparePassword(password);
+
+        if(!isMatch){
+          return res.status(401).json({ message: 'Invalid email or password'});
+        }
+
+        const token = user.generateAuthToken();
+
+        res.status(200).json({ token, user });
+    }
+    catch(error){
+      next(error);
+    }
+}
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -9,6 +9,12 @@ router.post("/register", [
     body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
     body('fullname.firstname').isLength({ min: 2 }).withMessage('First name must be at least 2 characters long'),
 ], 
-userController.register);
+userController.registerUser);
+
+router.get("/login",[
+    body('email').isEmail().withMessage('Invalid email'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+], 
+userController.loginUser);
 
 module.exports = router;
